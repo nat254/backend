b37b9ea9a2dfb949e4657ed3a63b1d447d717e53
nat254 <natalie.ndetei@strathmore.edu>
2024-04-15 21:41:42 +0300

    feat: add update functionality
    This allows us to change a specific name in an array.

--- controllers/cat.controller.js
+++ controllers/cat.controller.js
@@ -12,3 +12,16 @@ exports.create = (req, res) => {
 exports.read = (req, res) => {
     res.send(cats);
 }
+
+// Function to update a cat's name (using PUT method)
+exports.update = (req, res) => {
+    const { id, name } = req.params;
+
+    // Check if the index is within the bounds of the array
+    if (id >= 0 && id < cats.length) {
+        cats[id] = name;
+        res.send("Cat name updated");
+    } else {
+        res.status(404).send("Cat not found");
+    }
+}
