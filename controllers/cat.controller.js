b72212fcc37d05486119dd70de98235b44d4e759
nat254 <natalie.ndetei@strathmore.edu>
2024-04-15 21:44:02 +0300

    feat: add delete functionality
    This is to allow us to delete a specific item in the array.

--- controllers/cat.controller.js
+++ controllers/cat.controller.js
@@ -25,3 +25,15 @@ exports.update = (req, res) => {
         res.status(404).send("Cat not found");
     }
 }
+
+// Function to delete a cat (using DELETE method)
+exports.deleteCat = (req, res) => {
+    const index = req.params.index;
+    // Check if the index is within the bounds of the array
+    if (index >= 0 && index < cats.length) {
+        cats.splice(index, 1); // Remove the cat at the specified index
+        res.send("Cat deleted successfully");
+    } else {
+        res.status(404).send("Cat not found");
+    }
+}
